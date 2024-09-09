1a2fbea24a918cdfe0f5412ff38a4d2c66255586
andre00bejarano00vaca <andre.bejarano.vaca@gmail.com>
2024-09-09 00:32:44 -0400

    coneccion a la api

--- components/ImageList.jsx
+++ components/ImageList.jsx
@@ -9,7 +9,7 @@ const ImageList = () => {
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const response = await fetch('http://localhost:8080/api/images');
+        const response = await fetch('http://ccpa7stkruda3o.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com/api/images');
         if (!response.ok) {
           throw new Error('Failed to fetch images');
         }
